3a19d454145cffa2579cdb8c6290818f23ef11e5
vosamo <vosamo@outlook.com>
2016-04-25 17:22:29 +0800

    设置url名称

--- article/urls.py
+++ article/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^(?P<my_args>\d+)/$', views.detail),
+    url(r'^$', views.home, name='home'),
+    url(r'^(?P<my_args>\d+)/$', views.detail, name='detail'),
 ]
