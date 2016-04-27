8a82cbde710f99a8c2ac79e57563ecea25fc8c22
vosamo <vosamo@outlook.com>
2016-04-27 14:17:54 +0800

    添加多说评论系统

--- article/models.py
+++ article/models.py
@@ -1,11 +1,16 @@
 # coding:utf-8
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Article(models.Model):
     title = models.CharField(max_length=100)  # 文章题目
     category = models.CharField(max_length=50, blank=True)  # 文章标签
     date_time = models.DateTimeField(auto_now_add=True)  # 文章日期
     content = models.TextField(blank=True, null=True)  # 文章正文
+
+    def get_absolute_url(self):
+        path = reverse('detail', args=(1,))
+        return "http://127.0.0.1:8000%s" % path
 
     def __unicode__(self):
         return self.title
