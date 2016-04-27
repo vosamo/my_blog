e2913c48a5918a7031fc3fc8068f196d4787c7c1
vosamo <vosamo@outlook.com>
2016-04-27 08:50:58 +0800

    修改detail视图

--- article/views.py
+++ article/views.py
@@ -6,7 +6,7 @@ def home(request):
     post_list = Article.objects.all()
     return render(request, 'home.html', {'post_list': post_list})
 
-def detail(request, my_args):
-    post = Article.objects.all()[int(my_args)]
-    str = "title = %s, category = %s, date_time = %s, content = %s" % (post.title, post.category, post.date_time, post.content)
-    return HttpResponse(str)
+def detail(request, id):
+    post = Article.objects.get(id=str(id))
+    # str = "title = %s, category = %s, date_time = %s, content = %s" % (post.title, post.category, post.date_time, post.content)
+    return render(request, 'post.html', {'post': post})
